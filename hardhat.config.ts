2a2f171696d9e7768742c8277624e998a9c77b12
Nassim Dehouche <65496410+ndehouche@users.noreply.github.com>
2025-01-31 16:28:04 +0700

    Add files via upload

---
+++ hardhat.config.ts
@@ -0,0 +1,70 @@
+import type { HardhatUserConfig } from "hardhat/config";
+
+import "@matterlabs/hardhat-zksync";
+import * as dotenv from 'dotenv';
+WALLET_PRIVATE_KEY=your_private_key_here
+ETHERSCAN_SOPHON_API_KEY=your_sophscan_api_key_here  
+// Load environment variables from .env file
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "sophonTestnet",
+  networks: {
+    hardhat: {
+      zksync: true,
+    },
+    sophonMainnet: {
+      url: "https://rpc.sophon.xyz",
+      ethNetwork: "mainnet",
+      verifyURL: "https://verification-explorer.sophon.xyz/contract_verification",
+      browserVerifyURL: "https://explorer.sophon.xyz/",
+      enableVerifyURL: true,
+      zksync: true,
+      accounts: [process.env.WALLET_PRIVATE_KEY as string]
+    },
+    sophonTestnet: {
+      url: "https://rpc.testnet.sophon.xyz",
+      ethNetwork: "sepolia",
+      verifyURL: "https://api-explorer-verify.testnet.sophon.xyz/contract_verification",
+      browserVerifyURL: "https://explorer.testnet.sophon.xyz/",
+      enableVerifyURL: true,
+      zksync: true,
+      accounts: [process.env.WALLET_PRIVATE_KEY as string]
+    },
+  },
+  zksolc: {
+    version: "latest",
+    settings: {
+    },
+  },
+  solidity: {
+    version: "0.8.27",
+  },
+  etherscan: {
+    enabled: true,
+    apiKey: {
+      sophonTestnet: process.env.ETHERSCAN_SOPHON_API_KEY as string,
+      sophonMainnet: process.env.ETHERSCAN_SOPHON_API_KEY as string,
+    },
+    customChains: [
+      {
+        network: "sophonTestnet",
+        chainId: 531050104,
+        urls: {
+          apiURL: "https://api-testnet.sophscan.xyz/api",
+          browserURL: "https://testnet.sophscan.xyz",
+        },
+      },
+      {
+        network: "sophonMainnet",
+        chainId: 50104,
+        urls: {
+          apiURL: "https://api.sophscan.xyz/api",
+          browserURL: "https://sophscan.xyz",
+        },
+      },
+    ],
+  }
+};
+
+export default config;
